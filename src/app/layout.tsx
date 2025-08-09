2ab6fc7d7f72d5f8985178d600a3083d7e0bea14
MusashiSaito <mu97.job@gmail.com>
2025-08-09 17:49:18 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>{children}</body>
     </html>
   )
